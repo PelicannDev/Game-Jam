ee9bb76f8bf490166c6152d8d22d87b00e396c68
PelicannDev <pelicanndev@gmail.com>
2025-03-27 13:39:28 -0600

    base battery mechanic

--- game-jam/Scripts/player.gd
+++ game-jam/Scripts/player.gd
@@ -1,30 +1,46 @@
 extends CharacterBody2D
-
-var currPos = [0,0]
-
-func _input(event):
 	#Right Bound: 224 
 	#Left Bound: -48 
 	#Up Bound: -32 
 	#Down Bound: 160
-	
-	if event.is_action_pressed("right") && currPos[0] < 224:
-		currPos[0] += 16
-		get_node("Sprite2D").look_at(self.position + Vector2(1,0))
-		
-	elif event.is_action_pressed("left") && currPos[0] > -48:
-		currPos[0] -= 16
-		get_node("Sprite2D").look_at(self.position + Vector2(-1,0))
-		
-	elif event.is_action_pressed("up") && currPos[1] > -32:
-		currPos[1] -= 16
-		get_node("Sprite2D").look_at(self.position + Vector2(0,1))
-			
-	elif event.is_action_pressed("down") && currPos[1] < 160:
-		currPos[1] += 16
-		get_node("Sprite2D").look_at(self.position + Vector2(0,-1))
-		
-	self.position = Vector2(currPos[0], currPos[1])
 
-func _process(delta: float) -> void:
-	print(currPos[1])
+#player speed
+var speed = 100
+var can_move = true
+
+func _physics_process(delta):
+#setup direction of movement
+	var direction = Input.get_vector("left", "right", "up", "down")
+
+#stop diagonal movement by listening for input then setting axis to zero
+	if can_move:
+		if Input.is_action_pressed("right"):
+			direction.y = 0
+			get_node("AnimatedSprite2D").play("Right")
+
+		elif Input.is_action_pressed("left"):
+			direction.y = 0
+			get_node("AnimatedSprite2D").play("Left")
+
+		elif Input.is_action_pressed("up"):
+			direction.x = 0
+			get_node("AnimatedSprite2D").play("Up")
+
+		elif Input.is_action_pressed("down"):
+			direction.x = 0
+			get_node("AnimatedSprite2D").play("Down")
+
+		else:
+			direction = Vector2.ZERO
+
+#normalize the directional movement
+	direction = direction.normalized()
+
+#implement the actual movement
+	if can_move:
+		velocity = (direction * speed)
+		move_and_slide()
+	
+	#clamp position
+	position.x = clamp(position.x, 16, 304)
+	position.y = clamp(position.y, 16, 224)
